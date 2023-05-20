86c115890e2e08575ed25bcc049a7faa3b9d47aa
shiv kumar chawade <shvachawade89@mail.com>
2023-05-20 09:32:27 +0530

    first commit

---
+++ src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,15 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "shivachavde89@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "shivachawade89@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 19-may-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+}
